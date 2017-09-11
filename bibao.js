cd532c53fa2230b0a0f4d023c937e20dc0d922f8
zhiqiang.wen <zhiqiang.wen@ilaw66.com>
2017-09-11 10:40:50 +0800

    关于面向对象中的that与this的进一步理解，为什么别人非要用that的理由

---
+++ bibao.js
@@ -0,0 +1,33 @@
+/*代码片段一 B*/
+// var name = "The Window";
+
+// var object = {
+// 	name : "My Object",
+// 	getNameFunc : function(){
+// 		return function() {
+// 			return this.name;
+// 		};
+// 	}
+// };
+// alert(object.getNameFunc()()); // The Window; 
+/*代码片段一 E*/
+
+
+/*代码片段二 B*/
+// var name  = "The Window";
+// var object = {
+// 	name : "My Object",
+// 	getNameFunc :function(){
+// 		var that = this;
+// 		return function(){
+// 			return that.name;
+// 		};
+// 	}
+// }
+// alert(object.getNameFunc()()); // My Object
+/*代码片段二 E*/
+
+/*代码片段一 this与that的运用
+	防止调用者为全局变量
+	
+*/
